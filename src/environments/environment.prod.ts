6b643b159183f8a70c0347f9788082affb22aa83
Mahan Sharifi-Ghazvini <43676608+MahanSharifi@users.noreply.github.com>
2022-01-03 02:17:49 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://inspiring-quotes.p.rapidapi.com/random',
-  API_KEY: '17ce8a1f04msh4d6ff718c7bb4e8p114aafjsn0ab2e385bd68',
-  API_HOST: 'inspiring-quotes.p.rapidapi.com'
+  API_URL: 'ahttps://inspiring-quotes.p.rapidapi.com/random',
+  API_KEY: 'a17ce8a1f04msh4d6ff718c7bb4e8p114aafjsn0ab2e385bd68',
+  API_HOST: 'ainspiring-quotes.p.rapidapi.com'
 };
